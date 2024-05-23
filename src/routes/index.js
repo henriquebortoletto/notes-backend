a259ffe4c8719e3b4b9d42236d478adf82157fe0
Henrique Bortoletto <bortolettohenrique@gmail.com>
2024-05-23 19:32:27 -0300

    chore: adicionado schema do banco de dados atualizado

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,11 @@
 import { Router } from "express";
-import { usersRoutes } from "./users.routes.js";
+
+import { router as usersRoutes } from "./users.routes.js";
+import { router as notesRoutes } from "./notes.routes.js";
 
 const routes = Router();
+
 routes.use("/users", usersRoutes);
+routes.use("/notes", notesRoutes);
 
 export { routes };
